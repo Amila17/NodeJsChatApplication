1d42463ce641a980e53c48b69f3be6c4f66a9f66
Amila17 <amila.prabandhika@bedegaming.com>
2015-08-20 20:17:29 +0400

    Initial Commit

---
+++ NodeChatApp/public/js/index.js
@@ -0,0 +1,19 @@
+﻿
+var socket = io();
+$('#send-message-btn').click(function () {
+    
+    var msg = $('#message-box').val();
+
+    var dt = new Date();
+    var timestamp = dt.getDate() + "/" + dt.getMonth() + "/" + dt.getFullYear() + "T" + dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+
+    var fullMsg = timestamp + " : " + msg;
+    socket.emit('chat', fullMsg);
+    $('#messages').append($('<p>').text(fullMsg));
+    $('#message-box').val('');
+    return false;
+});
+
+socket.on('chat', function (msg) {
+    $('#messages').append($('<p>').text(msg));
+});
---
+++ NodeChatApp/sockets/base.js
@@ -0,0 +1,36 @@
+﻿
+var mongo = require('mongodb').MongoClient;
+
+module.exports = function (io) {
+    io.on('connection', function (socket) {
+        console.log('a user connected');
+        
+        socket.on('disconnect', function () {
+            console.log('user disconnected');
+        });
+        
+        socket.on('chat', function (msg) {
+
+            mongo.connect(process.env.MongoDbUri, function (err, db) {
+
+                var collection = db.collection('chat messages');
+                collection.insert({ content: msg }, function (err, o) {
+
+                    if (err) {
+                        console.warn(err.message);
+                    } else {
+                         console.log("chat message inserted into db: " + msg);
+                    }
+                });
+            });
+            
+            socket.broadcast.emit('chat', msg);
+        });
+        
+        mongo.connect(process.env.MongoDbUri, function (err, db) {
+            var collection = db.collection('chat messages');
+            var stream = collection.find().sort().limit(10).stream();
+            stream.on('data', function (chat) { socket.emit('chat', chat.content); });
+        });
+    });
+};
